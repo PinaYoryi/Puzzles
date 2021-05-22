c08e531eaf6003bb5a8ff446bd78d5f0f23da6bb
Eloy Moreno <eloymore@ucm.es>
2021-05-22 18:18:15 +0200

    Actualizado resources.bat para que sea como el de parkour
    Co-Authored-By: Daniel González Cerdeiras <49150080+danielcerdeiras@users.noreply.github.com>

--- PuzzlesGame/assets/resources.bat
+++ PuzzlesGame/assets/resources.bat
@@ -1,6 +1,6 @@
 cd ../../P32021/bin
 copy /Y resources.cfg "../../PuzzlesGame/bin/resources.cfg"
-cd ../../../../../assets/OgreResources
-echo [General] >> ../../bin/resources.cfg
-echo FileSystem=../assets/OgreResources >> ../../bin/resources.cfg
-for %%f in (*.zip) do echo Zip=../assets/OgreResources/%%~nxf >> ../../bin/resources.cfg
+cd ../PuzzlesGame/assets
+echo [General] >> ../bin/resources.cfg
+echo FileSystem=%CD%/resources >> ../bin/resources.cfg
+for /r %CD% %%f in (*.zip) do echo Zip=%CD%/resources/%%~nxf >> ../bin/resources.cfg
